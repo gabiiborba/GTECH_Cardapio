b40ed4bd561fea2739ddbc0cd8e43fa6a50cdfcf
Gabriela Borba <gborbas.gb@gmail.com>
2024-12-09 09:14:36 -0300

    Modificações

--- src/main/java/com/gtech/cardapio/repository/ProdutoPromocaoRepository.java
+++ src/main/java/com/gtech/cardapio/repository/ProdutoPromocaoRepository.java
@@ -1,7 +1,6 @@
 package com.gtech.cardapio.repository;
 
 import com.gtech.cardapio.model.ProdutoPromocao;
-import com.gtech.cardapio.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
